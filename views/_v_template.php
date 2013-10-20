6422465e352282d0f47d91a615975c48f16bfc45
Donna Wong <donnawwong@gmail.com>
2013-10-20 11:22:37 -0400

    Created master template view

--- views/_v_template.php
+++ views/_v_template.php
@@ -4,16 +4,40 @@
 	<title><?php if(isset($title)) echo $title; ?></title>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-					
+	<link rel="stylesheet" type="text/css" href="/css/main.css">				
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
 </head>
 
-<body>	
+<body>
+	<div id='menu'>
+		
+        <a href='/'>Home</a>
+
+        <!-- Menu for users who are logged in -->
+        <?php if($user): ?>
+
+            <a href='/users/logout'>Logout</a>
+            <a href='/users/profile'>Profile</a>
+
+        <!-- Menu options for users who are not logged in -->
+        <?php else: ?>
+
+            <a href='/users/signup'>Sign up</a>
+            <a href='/users/login'>Log in</a>
+
+        <?php endif; ?>
+
+    </div>
+	<div id="logo_top">
+		<a href="/"><img src="/images/meower_logo.png"/></a>
+	</div>
+	<div id="main_content">
+    <br>
 
 	<?php if(isset($content)) echo $content; ?>
-
+	</div>
 	<?php if(isset($client_files_body)) echo $client_files_body; ?>
 </body>
 </html>
