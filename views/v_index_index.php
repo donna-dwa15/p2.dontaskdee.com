b35b71f655c69b4fe497b156055a0fe15a185673
Donna Wong <donnawwong@gmail.com>
2013-10-20 11:22:15 -0400

    Created default main landing page view

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,11 +1,21 @@
-<p>
-	Hello World! You have successfully spawned a new application.
-</p>
-
-<p>
-	This message is being triggered via the c_index.php controller, within the index() method.
-</p>
-
-<p>
-	<strong>Since everything is in working order, you should now delete <?php echo APP_PATH?>diagnostics.php</strong>
-</p>
+<h1>Welcome to Meower!</h1>
+<form method="POST" action="/users/p_login">
+	<span id="login">Ready to meow? Login!</span><br/>
+	Email: <input type="text" maxlength="150" name="email"/><br/>
+	Password: <input type="password" maxlength="50" name="password"/><br/>
+	<input type="image" src="/images/login_btn.png" alt="login!"/>
+</form>
+<br/>
+<span id="new_user">
+	New user? Signup!
+</span>
+<br/>
+<a href="/users/signup"><img src="/images/Signup_btn.png" alt="signup!"/></a>
+<br/>
+<span id="break"><img src="/images/line.png"/></span>
+<br/>
+<div id="new_features">
+	New features!<br/>
+	<img src="/images/meower_head.png"/>Feeling lonely?  Search for new people to STALK!<br/>
+	<img src="/images/meower_head.png"/>Regret a meow?  SCRATCH those unwanted posts!<br/>
+</div>
