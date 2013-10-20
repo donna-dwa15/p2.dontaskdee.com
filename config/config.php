2daf790d6db87b408d9d51a689da871dc17a4b4c
Donna Wong <donnawwong@gmail.com>
2013-10-20 11:41:37 -0400

    Updated configurations

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@ If you want a different default time format for this app, set it below
 */
 
 # What is the name of this app?
-	define('APP_NAME', 'Sample App');  
+	define('APP_NAME', 'Meower');  
 
 # When email is sent out from the server, where should it come from?
 # Ideally, this should match the domain name
@@ -28,10 +28,10 @@ A email designated to receive messages from the server. Examples:
  	
  	Some might want this to be the same as the APP_EMAIL, others might want to create a designated gmail address for it
 */ 	
-	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
+	define('SYSTEM_EMAIL', 'donnawong@fas.harvard.edu'); 
 
 # Default DB name for this app
-	define('DB_NAME', "myapp_com"); 
+	define('DB_NAME', "p2_dontaskdee_com"); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
@@ -43,5 +43,5 @@ A email designated to receive messages from the server. Examples:
 	//define('SMTP_PASSWORD', '');
 
 # For extra security, you might want to set different salts than what the core uses
-	//define('PASSWORD_SALT', '');
-	//define('TOKEN_SALT', '');
+	define('PASSWORD_SALT', 'fCJPIQy^HB1rVcEZPTC8G-gaG)a*1))9bE^83BEz');
+	define('TOKEN_SALT', 'IK&TZeK&x!bM2FApzmOHgUG)e$)kOcV0wUKIs%gV');
