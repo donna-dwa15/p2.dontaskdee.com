6de71275c203a1c5cf0e11306a0b71b748f754be
Donna Wong <donnawwong@gmail.com>
2013-11-03 20:39:03 -0500

    Updated login page

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,19 +1,21 @@
-<form method='POST' action='/users/p_login'>
-	<?php if(isset($error)): ?>
-        <div class='error'>
-            Login failed. Please double check your email and password.
-        </div>
-        <br>
-    <?php endif; ?>
-	<span id="login">Login!</span><br/>
-	Email<br/>
-	<input type="text" size="40" maxlength="150" name="email"/><br/>
-	Password<br/> 
-	<input type="password" size="40" maxlength="50" name="password"/><br/>
-	<input type="image" src="/images/login_btn.png" alt="login!"/>	
-</form>
-<span id="new_user">
-	New user? Signup!
-</span>
-<br/>
-<a href="/users/signup"><img src="/images/signup_btn.png" alt="signup!"/></a>
+<div id="form_content">
+	<h1>Login to start meowing!</h1>
+	<form method="POST" action="/users/p_login">
+		<?php if(isset($error)): ?>
+			<div class="error">
+				<?=$_SESSION['error']?>
+			</div>
+			<br>
+		<?php endif; ?>
+		<label for="email">Email</label><br/>
+		<input type="text" size="30" maxlength="150" name="email" value="<?=$_SESSION['email']?>"/><br/>
+		<label for="password">Password</label><br/> 
+		<input type="password" size="30" maxlength="50" name="password"/><br/>
+		<input type="image" src="/images/login_btn.png" alt="login!"/>	
+	</form>
+	<span id="new_user">
+		New user? Sign up!
+	</span>
+	<br/>
+	<a href="/users/signup"><img src="/images/signup_btn.png" alt="sign up!"/></a>
+</div>
