558965072f348141655ad2ad3482b1b144105530
Donna Wong <donnawwong@gmail.com>
2013-11-03 20:40:05 -0500

    Updated index controller formatting

--- controllers/c_index.php
+++ controllers/c_index.php
@@ -1,37 +1,47 @@
 <?php
 
-class index_controller extends base_controller {
+class index_controller extends base_controller 
+{
 	
 	/*-------------------------------------------------------------------------------------------------
 
 	-------------------------------------------------------------------------------------------------*/
-	public function __construct() {
+	public function __construct() 
+	{
 		parent::__construct();
 	} 
 		
 	/*-------------------------------------------------------------------------------------------------
 	Accessed via http://localhost/index/index/
 	-------------------------------------------------------------------------------------------------*/
-	public function index() {
+	public function index() 
+	{
 		
-		# Any method that loads a view will commonly start with this
-		# First, set the content of the template with a view file
+		# Check if user is logged in
+		# If not logged in, view main landing page with signup/login
+		# Else redirect to users index page
+		if(!isset($this->user->token))
+		{
+			# Set template data
 			$this->template->content = View::instance('v_index_index');
-			
-		# Now set the <title> tag
 			$this->template->title = "Welcome to ".APP_NAME;
 	
-		# CSS/JS includes
+			# CSS/JS includes
 			/*
-			$client_files_head = Array("");
-	    	$this->template->client_files_head = Utils::load_client_files($client_files);
-	    	
-	    	$client_files_body = Array("");
-	    	$this->template->client_files_body = Utils::load_client_files($client_files_body);   
+				$client_files_head = Array("");
+				$this->template->client_files_head = Utils::load_client_files($client_files);
+				
+				$client_files_body = Array("");
+				$this->template->client_files_body = Utils::load_client_files($client_files_body);   
 	    	*/
-	      					     		
+		}
+		else
+		{
+			# Redirect user to their index page
+			Router::redirect("/users/index/");
+		}
 		# Render the view
-			echo $this->template;
+		echo $this->template;
 
 	} # End of method
 	
