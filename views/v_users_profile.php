97ea6d5b4bee2c05a0778626f192e5dc68ca4290
Donna Wong <donnawwong@gmail.com>
2013-11-03 20:39:15 -0500

    Updated test content

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,1 +1,14 @@
-<h1>This is the profile of <?=$user->first_name?></h1>
+<div id="profile">
+	<!-- Profile header depending on who is viewing -->
+	<h1><?=$header?></h1>
+	<!-- Profile content -->
+	<div id="profile_info">
+		<?php if(!isset($error)): ?>
+			Name: <?=$first_name?> <?=$last_name?><br/>
+			Email: <?=$email?><br/>
+		<?php endif; ?>
+		<?php if(isset($last_post)): ?>
+			Last Meow: <?=$last_post?><br/>
+		<?php endif; ?>
+	</div>
+</div>
