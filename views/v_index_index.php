d9bdf1d25380beff8605a8f7ee47622e41d7ade9
Donna Wong <donnawwong@gmail.com>
2013-11-05 00:04:38 -0500

    Fixed form labels and added alt attributes

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -2,13 +2,13 @@
 <!-- Left hand content -->
 <div id="new_features">
 	<h2>New features!</h2>
-	<img src="/images/meower_head.png"/>Feeling lonely?  Search for new people to STALK!<br/>
-	<img src="/images/meower_head.png"/>Regret a meow?  SCRATCH those unwanted posts!<br/>
+	<img src="/images/meower_head.png" alt=""/>Feeling lonely?  Search for new people to STALK!<br/>
+	<img src="/images/meower_head.png" alt=""/>Regret a meow?  SCRATCH those unwanted posts!<br/>
 	<h2>Find your prey today!</h2>
 </div>
 <!-- Content divider -->
 <div id="vert_line">
-	<img src="/images/v_line.png"/>
+	<img src="/images/v_line.png" alt=""/>
 </div>
 <!-- Right hand content -->
 <div id="form_content">
@@ -18,11 +18,11 @@
 			<h2>Ready to meow? Login!</h2>
 		</header>
 		<label for="email">Email</label><br/>
-		<input type="text" size="30" maxlength="150" name="email"/>
+		<input type="text" size="30" maxlength="150" id="email" name="email"/>
 		<br/>
 		<label for="password">Password</label>
 		<br/>
-		<input type="password" size="30" maxlength="50" name="password"/>
+		<input type="password" size="30" maxlength="50" id="password" name="password"/>
 		<br/>
 		<input type="image" src="/images/login_btn.png" alt="login!"/>
 	</form>
