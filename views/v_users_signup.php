b856f4edf65aa9dbd182ce38f330b20a38ea3ec8
Donna Wong <donnawwong@gmail.com>
2013-11-05 00:05:51 -0500

    Fixed form labels

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -16,19 +16,19 @@
 		<?php endif; ?>
 		<!-- User info fields -->
 		<label for="first_name">First Name</label><br/>
-		<input type="text" name="first_name" size="30" maxlength="150" value="<?=$_SESSION['first_name']?>"/>
+		<input type="text" id="first_name" name="first_name" size="30" maxlength="150" value="<?=$_SESSION['first_name']?>"/>
 		<br/>
 		<label for="last_name">Last Name</label><br/>
-		<input type="text" name="last_name" size="30" maxlength="150" value="<?=$_SESSION['last_name']?>"/>
+		<input type="text" id="last_name" name="last_name" size="30" maxlength="150" value="<?=$_SESSION['last_name']?>"/>
 		<br/>
 		<label for="email">Email</label><br/>
-		<input type="text" name="email" size="30" maxlength="150" value="<?=$_SESSION['email']?>"/>
+		<input type="text" id="email" name="email" size="30" maxlength="150" value="<?=$_SESSION['email']?>"/>
 		<br/>
 		<label for="password">Password</label><br/>
-		<input type="password" name="password" size="30" maxlength="50" value="<?=$_SESSION['password']?>"/>
+		<input type="password" id="password" name="password" size="30" maxlength="50" value="<?=$_SESSION['password']?>"/>
 		<br/>
 		<label for="password_confirm">Confirm Password</label><br/>
-		<input type="password" name="password_confirm" size="30" maxlength="50" value="<?=$_SESSION['password_confirm']?>"/>
+		<input type="password" id="password_confirm" name="password_confirm" size="30" maxlength="50" value="<?=$_SESSION['password_confirm']?>"/>
 		<br/>	
 		<input type="image" src="/images/signup_btn.png" alt="sign up!"/>
 	</form>
