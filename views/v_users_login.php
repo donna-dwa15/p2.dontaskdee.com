72cb5d892b532c02973fbd5a90a4cc6a1747b72b
Donna Wong <donnawwong@gmail.com>
2013-11-05 00:05:39 -0500

    Fixed form labels

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,16 +1,18 @@
 <div id="form_content">
-	<h1>Login to start meowing!</h1>
+	<header>
+		<h1>Login to start meowing!</h1>
+	</header>
 	<form method="POST" action="/users/p_login">
 		<?php if(isset($error)): ?>
 			<div class="error">
 				<?=$_SESSION['error']?>
 			</div>
-			<br>
+			<br/>
 		<?php endif; ?>
 		<label for="email">Email</label><br/>
-		<input type="text" size="30" maxlength="150" name="email" value="<?=$_SESSION['email']?>"/><br/>
+		<input type="text" size="30" maxlength="150" id="email" name="email" value="<?=$_SESSION['email']?>"/><br/>
 		<label for="password">Password</label><br/> 
-		<input type="password" size="30" maxlength="50" name="password"/><br/>
+		<input type="password" size="30" maxlength="50" id="password" name="password"/><br/>
 		<input type="image" src="/images/login_btn.png" alt="login!"/>	
 	</form>
 	<span id="new_user">
