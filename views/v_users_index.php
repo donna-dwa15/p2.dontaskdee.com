173fc6d09cbd0e55d415c44afd97c1cbdfcbe6b2
Donna Wong <donnawwong@gmail.com>
2013-11-05 22:43:14 -0500

    Added user specific stats and random post

--- views/v_users_index.php
+++ views/v_users_index.php
@@ -1,4 +1,31 @@
-<h1>Welcome back, <?=$user->first_name?>!</h1>
-<br/>
-<p>
-</p>
+<h1>Hello, <?=$user->first_name?>!</h1>
+<div id="users">
+	<?php if(isset($last_login)): ?>
+	Last logged in on 
+	<time datetime="<?=Time::display($last_login,'Y-m-d G:i',$user->timezone)?>">
+		<?=Time::display($last_login,'F j, Y g:ia',$user->timezone)?>
+	</time>.
+	<br/><br/>
+	<?php endif; ?>
+	You are currently stalking <?=$followed?> prey.<br/>
+	You are being stalked by <?=$following?> predators.<br/>
+	<br/>
+	<?php if(isset($random_post)): ?>
+	One of your prey meowed:<br/>
+	<article>
+		<!-- Print the name of the post's creator and date created -->
+		<span class="user_info">
+			<?=$random_post['first_name']?> <?=$random_post['last_name']?> posted on 
+			<time datetime="<?=Time::display($random_post['created'],'Y-m-d G:i',$user->timezone)?>">
+				<?=Time::display($random_post['created'],'F j, Y g:ia',$user->timezone)?>
+			</time>
+		</span>		
+		<!-- Display Unfollow Button if post is not from current user -->
+		<a href="/posts/post_unfollow/<?=$random_post['post_user_id']?>"><img src="/images/unstalk_btn_2.png" alt="Unstalk!"/></a>	
+		<!-- Display post content -->
+		<p><?=$random_post['content']?></p>
+	</article>
+	<? else: ?>
+		Everything is quiet.<br/>
+	<?php endif; ?>
+</div>
